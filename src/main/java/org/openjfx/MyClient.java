25f00c8e7aa21ba1b9619a26821be7401f8627c3
Annebreizh <anne.couapel@ensea.fr>
2022-11-18 09:42:29 +0100

    Primary Controller : Envoi d'une valeur à mon voisin de classe MyChart : tracer d'un graphe avec des valeurs aléatoires MyHeader : barre pour le titre ChartUpdater fonctionne'

--- src/main/java/org/openjfx/MyClient.java
+++ src/main/java/org/openjfx/MyClient.java
@@ -6,7 +6,7 @@ import java.net.*;
 public class MyClient {
     public static void main(String[] args){
         try{
-            Socket s= new Socket("localhost",6666);
+            Socket s= new Socket("10.10.25.85",6666);
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
             dout.writeUTF("0.23");
             dout.flush();
--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -1,6 +1,9 @@
 package org.openjfx;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.Socket;
+
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -18,4 +21,18 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    @FXML
+    public void addClient() throws IOException {
+        try{
+            Socket s= new Socket("10.10.25.85",6666);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF(String.valueOf(Math.random()));
+            dout.flush();
+            dout.close();
+            s.close();
+        } catch (Exception e){
+            System.err.println(e);}
+    }
 }
+
